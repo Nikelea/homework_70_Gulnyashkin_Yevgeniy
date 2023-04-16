c46e5cdb9dd572388d8db429800e7edd07b453d4
Гульняшкин Евгений <ducat.ea@gmail.com>
2023-04-16 19:11:46 +0600

    Добавленны вью для задач по аналогии с проектами.

--- api_v1/views.py
+++ api_v1/views.py
@@ -42,3 +42,39 @@ class ProjectDetailView(APIView):
         object = get_object_or_404(TodoProject, pk=kwargs.get('pk'))
         object.delete()
         return Response({'deleted': kwargs.get('pk') })
+
+
+
+class TaskListView(APIView):
+    def get(self, request, *args, **kwargs):
+        objects = TodoTask.objects.all()
+        serializer = TaskSerializer(objects, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+class TaskDetailView(APIView):
+    def get(self, request, *args, **kwargs):
+        objects = TodoTask.objects.filter(id=kwargs.get('pk'))
+        serializer = TaskSerializer(objects, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        object = get_object_or_404(TodoTask, pk=kwargs.get('pk'))
+        serializer = TaskSerializer(object, data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+    def delete(self, request, *args, **kwargs):
+        object = get_object_or_404(TodoTask, pk=kwargs.get('pk'))
+        object.delete()
+        return Response({'deleted': kwargs.get('pk') })
